52244370e451c513004232ac2dc72183212152f8
Andrii Falieiev <falieievandrii@gmail.com>
2022-07-21 21:25:56 +0300

    added a STRONG ATTACK functionality (71)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,40 @@
-const ATTACK_VALUE = 10;      //hardcoded global variable 
+const ATTACK_VALUE = 10; //hardcoded global variable
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
-let chosenMaxLife = 100;   //later it will be user input
+let chosenMaxLife = 100; //later it will be user input
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler () {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
-    currentMonsterHealth -= damage;
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost...");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("It's a draw.");
+  }
 }
 
-attackBtn.addEventListener('click', attackHandler);
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
