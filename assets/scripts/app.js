518934e0dca6221512658e38d6d31abd61e5545a
Andrii Falieiev <falieievandrii@gmail.com>
2022-07-22 16:53:41 +0300

    added Validating User Input l.75

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,21 +3,52 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100; //later it will be user input
+// const enteredValue = parseInt(prompt("Maximum life for you and the monster.", "100"));
+//or use parseInt with variable
+const enteredValue = prompt("Maximum life for you and the monster.", "100");
+
+let chosenMaxLife = parseInt(enteredValue); //later it will be user input
+//check if the user entered a number (not NaN, not zero and not negative)
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+} 
+
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+
+
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+if (currentPlayerHealth <= 0 && hasBonusLife) {//hasBonusLife, it'll check does it true?
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    alert("You would be dead, but the bonus life saved you!");
+    setPlayerHealth(initialPlayerHealth);   //update the player health bar
+}
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You won!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert("You lost...");
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert("It's a draw.");
+  } 
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+    reset();
   }
 }
 
