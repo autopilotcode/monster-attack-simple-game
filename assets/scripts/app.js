56f2a23dc484c8ba07a24736dc1127c2fea97f87
Andrii Falieiev <falieievandrii@gmail.com>
2022-07-22 18:49:38 +0300

    finished Global Constants as Identifiers in Conditional Code l.76

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK = "ATTACK"; //MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = "STRONG_ATTACK"; //MODE_STRONG_ATTACK = 1
+
 // const enteredValue = parseInt(prompt("Maximum life for you and the monster.", "100"));
 //or use parseInt with variable
 const enteredValue = prompt("Maximum life for you and the monster.", "100");
@@ -12,14 +15,11 @@ let chosenMaxLife = parseInt(enteredValue); //later it will be user input
 
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
-} 
-
+}
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
-
-
 
 adjustHealthBars(chosenMaxLife);
 
@@ -33,20 +33,21 @@ function endRound() {
   const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
-if (currentPlayerHealth <= 0 && hasBonusLife) {//hasBonusLife, it'll check does it true?
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    //hasBonusLife, it'll check does it true?
     hasBonusLife = false;
     removeBonusLife();
     currentPlayerHealth = initialPlayerHealth;
     alert("You would be dead, but the bonus life saved you!");
-    setPlayerHealth(initialPlayerHealth);   //update the player health bar
-}
+    setPlayerHealth(initialPlayerHealth); //update the player health bar
+  }
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You won!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert("You lost...");
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert("It's a draw.");
-  } 
+  }
   if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
     reset();
   }
@@ -54,9 +55,9 @@ if (currentPlayerHealth <= 0 && hasBonusLife) {//hasBonusLife, it'll check does 
 
 function attackMonster(mode) {
   let maxDamage;
-  if (mode === "ATTACK") {
+  if (mode === MODE_ATTACK) {
     maxDamage = ATTACK_VALUE;
-  } else if (mode === "STRONG_ATTACK") {
+  } else if (mode === MODE_STRONG_ATTACK) {
     maxDamage = STRONG_ATTACK_VALUE;
   }
   const damage = dealMonsterDamage(maxDamage);
@@ -65,20 +66,20 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-  attackMonster("ATTACK");
+  attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-  attackMonster("STRONG_ATTACK");
+  attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
   let healValue;
   if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
     alert("You can't hela to more then your max initial health.");
-    healValue = chosenMaxLife - currentPlayerHealth
+    healValue = chosenMaxLife - currentPlayerHealth;
   } else {
-      healValue = HEAL_VALUE;
+    healValue = HEAL_VALUE;
   }
   increasePlayerHealth(HEAL_VALUE);
   currentPlayerHealth += HEAL_VALUE;
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,11 +1,11 @@
-const monsterHealthBar = document.getElementById('monster-health');
-const playerHealthBar = document.getElementById('player-health');
-const bonusLifeEl = document.getElementById('bonus-life');
+const monsterHealthBar = document.getElementById("monster-health");
+const playerHealthBar = document.getElementById("player-health");
+const bonusLifeEl = document.getElementById("bonus-life");
 
-const attackBtn = document.getElementById('attack-btn');
-const strongAttackBtn = document.getElementById('strong-attack-btn');
-const healBtn = document.getElementById('heal-btn');
-const logBtn = document.getElementById('log-btn');
+const attackBtn = document.getElementById("attack-btn");
+const strongAttackBtn = document.getElementById("strong-attack-btn");
+const healBtn = document.getElementById("heal-btn");
+const logBtn = document.getElementById("log-btn");
 
 function adjustHealthBars(maxLife) {
   monsterHealthBar.max = maxLife;
@@ -14,8 +14,9 @@ function adjustHealthBars(maxLife) {
   playerHealthBar.value = maxLife;
 }
 
-function dealMonsterDamage(damage) {  //calculates random amount of damage & adjuct moster health bar
-  const dealtDamage = Math.random() * damage;         
+function dealMonsterDamage(damage) {
+  //calculates random amount of damage & adjuct moster health bar
+  const dealtDamage = Math.random() * damage;
   monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
   return dealtDamage;
 }
